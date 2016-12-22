5bed5f3bacb6b54495bb1c66731d5f25cbcf4cae
johnsi-18cse <johnsijohn18@gmail.com>
2016-12-22 11:30:25 +0530

    Add files via upload

--- alp.c
+++ alp.c
@@ -6,8 +6,8 @@ char ch,a,Z,A,z;
 printf("\n enter the char");
 scanf("%c",&ch);
 if((ch>='a' && ch<='z')||(ch>='A' && ch<='Z'))
-printf("\n alphabet %c",ch);
+printf("\n alphabet ");
 else
-printf("\n not alphabet %c",ch);
+printf("\n not alphabet ");
 getch();
 }
